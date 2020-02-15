b1836efb07811dd77d941b99470d384a926942cb
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-02-15 09:34:32 -0300

    fix google call add encode uri component

--- serverless/services/google-geolocation.ts
+++ serverless/services/google-geolocation.ts
@@ -4,5 +4,7 @@ const GOOGLE_API_KEY = 'AIzaSyBBHfDUD4haTmK5rVl0iNydDlsry6KU0aM';
 
 export const getAddressFromGoogle = (address: string) =>
   axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_API_KEY}`
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${GOOGLE_API_KEY}`
   );
